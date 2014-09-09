c4de29467538090a40ef2fdd300f7ce8ecd58ec4
Mark <Mark@145.48.231.33>
2014-09-09 11:14:45 +0200

    HAHA Git =Awesome

--- factuur/src/factuur/main/Main.java
+++ factuur/src/factuur/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Gregor is gek");
+System.out.println("Gregor pro in Git");
 	}
 
 }
